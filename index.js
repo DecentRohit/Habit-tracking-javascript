9255687ec237adc7d5264adc3cd44e720b2e1031
Rohit Soni <143289308+DecentRohit@users.noreply.github.com>
2024-07-08 21:49:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const connectUsingMongoose = require("./config/mongooseConfig")
 const bodyParser = require('body-parser');
