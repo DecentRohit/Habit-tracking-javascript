7c1dfa35eb64c4f85108485adec9de1e011abccf
Rohit Soni <143289308+DecentRohit@users.noreply.github.com>
2024-07-08 20:30:06 +0530

    Update mongooseConfig.js
    changed url to connect

--- config/mongooseConfig.js
+++ config/mongooseConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const Url = 'mongodb://127.0.0.1:27017/habitTracker'
+const Url = 'mongodb://0.0.0.0:27017/habitTracker'
 
  const connectUsingMongoose = async ()=>{
     try{await  mongoose.connect(Url)
